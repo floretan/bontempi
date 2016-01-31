fd65cdc5762c4d3822e7a311706243f912c99ad3
Florian Lorétan <florian.loretan@wunderkraut.com>
2016-01-31 21:11:48 +0100

    Update conversion script.

--- experiments/sampler_test/convert.sh
+++ experiments/sampler_test/convert.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-# Convert to raw.
-#for f in ./*; 
-#do 
-#  sox $f -b 16 -c 1 "$f.raw"; 
-#done
+# We start with C1, which is midi note 12.
+offset=11;
 
+for source in ./*.wav;
+do
+  name=`basename -s .wav $source`
+  mkdir -p raw_samples/$name
+  rm raw_samples/$name/*
 
-# Split one file
-sox celesta.wav -b 16 -c 1 aac/celesta-%1n.wav trim 0 8 : newfile : restart
+  # Split one file
+  sox $source -b 16 -c 1 raw_samples/$name/%1n.raw trim 0 8 : newfile : restart
+  
+  # with u-law
+  # sox $source -b 16 -c 1 -e u-law raw_samples/$name/%1n.raw trim 0 8 : newfile : restart
 
-# Rename files to have each suffix match the midi note
-for i in {48..0}; do a=$(($i+10)); 
-  # Set the right midi note numbers
-  mv "aac/celesta-$i.wav" "aac/celesta-$a.wav"; 
-  ffmpeg -i "aac/celesta-$a.wav" -b:a 96k "aac/celesta-$a.aac"
-  rm "aac/celesta-$a.wav"
+  # Rename files to have each suffix match the midi note
+  for i in {48..1}; do a=$(($i+$offset)); 
+    # Set the right midi note numbers
+    mv "raw_samples/$name/$i.raw" "raw_samples/$name/$a.raw"
+#    ffmpeg -i "aac/celesta-$a.wav" -b:a 96k "aac/CEL$a.aac"
+#    rm "aac/celesta-$a.wav"
+  done
 done
